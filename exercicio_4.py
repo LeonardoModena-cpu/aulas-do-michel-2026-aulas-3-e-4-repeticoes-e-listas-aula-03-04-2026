33dbd8a6ed19189a9feea94ac12f6f99dfc777c6
LeonardoModena-cpu <leomodena@hotmail.com>
2026-05-12 20:34:28 -0300

    Update exercicio_4.py

--- exercicio_4.py
+++ exercicio_4.py
@@ -27,10 +27,27 @@ Digite a sua segunda lista (separando os números por vírgula): 9, 9, 9, 30
 
 Resposta:
 Ambas
-"""
+
 
 # Código para pegar as listas de input
 primeira_lista = [*map(int, input("Digite a sua primeira lista (separando os números por vírgula): ").split(","))]
 segunda_lista = [*map(int, input("Digite a sua segunda lista (separando os números por vírgula): ").split(","))]
 
 # Fazer a partir daqui
+"""
+
+primeira_lista = [*map(int, input("Digite a sua primeira lista (separando os números por vírgula): ").split(","))]
+segunda_lista = [*map(int, input("Digite a sua segunda lista (separando os números por vírgula): ").split(","))]
+
+maior_primeira = max(primeira_lista)
+maior_segunda = max(segunda_lista)
+
+if maior_primeira > maior_segunda:
+    print("Primeira")
+
+elif maior_segunda > maior_primeira:
+    print("Segunda")
+
+else:
+    print("Ambas")
+  
